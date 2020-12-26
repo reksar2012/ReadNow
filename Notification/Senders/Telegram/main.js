2e081b169be101d22f256e1756b1727c71c9717f
Sergei Stenchev <stenchev60@gmail.com>
2020-12-27 00:22:15 +0300

    Переписал модуль отправки сообщений

--- Notification/Senders/Telegram/main.js
+++ Notification/Senders/Telegram/main.js
@@ -1,17 +1,35 @@
-import TelegramBot from "node-telegram-bot-api"
-import Vault from "./vault.mjs";
-var init = async _ =>{
-    //let credsResponce = await Vault.read("rabbitmq/creds/administrtor")
-    let configResponce = await Vault.read("kv/sender/telegram")
-    return {...configResponce.data}
-}
-var config = await init();
+const TelegramBot = require("node-telegram-bot-api");
+const ngrok = require('ngrok');
+const express = require('express');
 
-const bot = new TelegramBot(config.Token,{
-    webHook:{
-                port: config.Port   
-            }
-});
-bot.setWebHook(`https://522c515fe600.ngrok.io/bot/${config.Token}`)
+const id = 129816288;
 
-bot.sendMessage(config.ChatId,"2")
+async function Start() {
+    const app = express();
+    app.use(express.json());
+    const url = await ngrok.connect(90);
+    try {      
+    const bot = new TelegramBot("1472617178:AAHHwaLKCnboJwPKL4imgJt-DqGmffrdN3c", {
+      webHook: {
+        port: 90
+      }
+    });
+
+    bot.setWebHook(`${url}/bot/1472617178:AAHHwaLKCnboJwPKL4imgJt-DqGmffrdN3c"`)
+    
+    app.post("/send", (req, res) => {
+      console.log(req.body.message);
+      bot.sendMessage(id, req.body.message);
+      res.sendStatus(200)
+    })
+    app.listen(3000);    
+    }
+    catch(e)
+    {
+      console.log(e);
+    } 
+};
+
+Start();
+
+

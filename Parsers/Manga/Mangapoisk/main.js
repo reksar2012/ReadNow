8abd798284a6773f17a3a40a277d5345fd6ea46a
Sergei Stenchev <stenchev60@gmail.com>
2020-12-27 00:24:21 +0300

    добавил поддержку docker

--- Parsers/Manga/Mangapoisk/main.js
+++ Parsers/Manga/Mangapoisk/main.js
@@ -1,166 +1,209 @@
 const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs').promises;
-
+const parceMode= process.env.ParceMode?.split(",") || ["parce"];
 const path = require("path");
 const cheerio = require('cheerio');
+const hash = require("object-hash")
 let uas = [];
 
-const queues = ['manga','chaters'];
- 
+const queues = ['manga', 'chaters'];
+console.log(parceMode);
+
 var amqp = require('amqplib/callback_api');
 let RabbitMqChannel;
 const url = 'mongodb://root:admin@192.168.31.174:27017';
-var client = new MongoClient(url);
-client.connect({ useUnifiedTopology: true })
+var client = new MongoClient(url,{useUnifiedTopology:true});
+client.connect()
 // Database Name
 const dbName = 'ReadNow';
- 
-amqp.connect('amqp://192.168.31.174:5672', function(error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
+
+amqp.connect('amqp://192.168.31.174:5672', function (error0, connection) {
+    if (error0) {
+        throw error0;
     }
-    
-    queues.forEach(x=>channel.assertQueue(x, {
-      durable: false,      
-    }));
-    channel.consume(queues[0],ParceManga,{noAck: true});
-    channel.consume(queues[1],ParceChater,{noAck: true})
-    RabbitMqChannel = channel;
-    
-  });
-});  
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
 
-async function Parce(){
-    
-   console.time('Full parce');
+        queues.forEach(x => {channel.assertExchange(x,'direct', {
+            durable: false,
+        });
+        channel.bindQueue(x, x, "mangapoisk");
+        }
+        );
 
-   uas=await GetUAs();
-   
-   console.time('Category parce');
-   await ParceCategory();
-   console.timeEnd('Full parce');
+
+        if(parceMode.includes("listening"))
+        {
+            channel.assertQueue(queues[0],{exclusive:true},ParceManga);
+            channel.assertQueue(queues[1],{exclusive:true},ParceChater)
+        }
+        RabbitMqChannel = channel;
+
+    });
+});
+
+async function Parce() {
+
+    console.time('Full parce');
+
+    uas = await GetUAs();
+
+    console.time('Category parce');
+    await ParceCategory();
+    console.timeEnd('Full parce');
 
 }
 
 async function ParceChater(chaterObj) {
-    if(chaterObj!=undefined){
+    if (chaterObj != undefined) {
         var chater = JSON.parse(chaterObj.content.toString());
     }
-    else{
+    else {
         return;
     }
     await GetChattersImage(chater);
+    RabbitMqChannel.ack(chaterObj)
 }
 
 async function ParceManga(mangaObj) {
-    if(mangaObj!=undefined){
+    if (mangaObj != undefined) {
         var manga = JSON.parse(mangaObj.content.toString());
-        if(manga == undefined){
+        if (manga == undefined) {
             return;
         }
     }
-    else{
+    else {
         return;
     }
-    manga = await GetTitleInfo(manga);
-    manga = await GetChatters(manga);
-    try{
-    await client.db(dbName)
-        .collection("Manga")
-        .updateOne({
-            Url: manga.url,
-            Name: manga.name
-        },
-        {$set:{
-            Url: manga.url,
-            Img: manga.img,
-            Name: manga.name,
-            LikeCount: manga.LikeCount,
-            Rating:manga.Rating,
-            Status: manga.Status,
-            Categories: manga.Categories,
-            Description:manga.Description,
-            Year:manga.Year
-        }},
-        {
-            upsert:true
-        });
+    try {
+        console.log("start parsing ");
+        manga = await GetTitleInfo(manga);
+        manga = await GetChatters(manga);
+
+        await client.db(dbName)
+            .collection("Manga")
+            .updateOne({
+                Url: manga.url,
+                Name: manga.name
+            },
+                {
+                    $set: {
+                        Url: manga.Url,
+                        Img: manga.Img,
+                        Name: manga.Name,
+                        LikeCount: manga.LikeCount,
+                        Rating: manga.Rating,
+                        Status: manga.Status,
+                        Categories: manga.Categories,
+                        Description: manga.Description,
+                        Year: manga.Year
+                    }
+                },
+                {
+                    upsert: true
+                });
     }
-    catch(e){
-        console.dir(manga)
-        console.log(e)
+    catch (e) {
+        console.dir(manga);
+        console.log(e);
+        return;
     }
+    console.dir(manga)
+    console.log("end parsing "+manga.name);
 }
 
 async function ParceCategory() {
     let categories = await GetCategories();
-    console.timeEnd('Category parce');
     let mangas = [];
 
     await Promise.all(categories.map(x => {
         return GetTitle(x).then(elements => mangas = [...mangas, ...elements]);
     }));
-    mangas.map(x=>
-        RabbitMqChannel.sendToQueue(
-            queues[0],
-            Buffer.from(JSON.stringify(x))));;
+
+    mangas = mangas.map(x=>{
+        var objectHash = hash.sha1(x);
+        x.hash = objectHash;
+        return x
+    });
+
+    var hashes =[ ...new Set(mangas.map(x=>x.hash))];
+
+    mangas = mangas.filter(x=>{
+        var result = hashes.includes(x.hash);
+        if(result) 
+        hashes = hashes.filter(h=>h!=x.hash)
+        return result;
+    }).map(x=>{return {Name: x.name,Url:x.url}})
+    .map(x=>RabbitMqChannel.publish(
+        queues[0],
+        "mangapoisk",
+        Buffer.from(
+            JSON.stringify(x)
+            )
+        )
+    );
+    
 }
 
 async function GetCategories() {
-        let ua = await GetRandomUA();
-        var response = await axios(
-                                        `https://mangapoisk.ru/genre`,
-                                        {
-                                            headers: 
-                                                {
-                                                    'User-Agent': ua 
-                                                }
-                                        });
-        const html = response.data;
-        const $ = cheerio.load(html);
-         
+    let ua = await GetRandomUA();
+    var response = await axios(
+        `https://mangapoisk.ru/genre`,
+        {
+            headers:
+            {
+                'User-Agent': ua
+            }
+        });
+    const html = response.data;
+    const $ = cheerio.load(html);
+
     return $('.col-6.col-md-4.py-2 a')
-                .toArray()
-                .map(x => {
-                    return {
-                        url: "https://mangapoisk.ru" + x.attribs.href,
-                        name: x.firstChild.nodeValue
-                    };
-                });
+        .toArray()
+        .map(x => {
+            return {
+                url: "https://mangapoisk.ru" + x.attribs.href,
+                name: x.firstChild.nodeValue
+            };
+        });
 }
 
 async function GetTitle(category) {
     let manga = [];
     let elements = [];
     let offset = 1;
+    isSuccess = true
     do {
-        elements = [];
         let ua = await GetRandomUA();
-        try{
-        var response = await axios(`${category.url}?page=${offset}`,{
-            headers: 
-                {
-                    'User-Agent':  ua
-                }
-        });
+        let isSuccess = true;
+        do {
+
+            try {
+                isSuccess = true;
+                var response = await axios(`${category.url}?page=${offset}`, {
+                    headers:
+                    {
+                        'User-Agent': ua
+                    }
+                });
+            }
+            catch {
+                isSuccess = false;
+                await delay(5000);
+            }
         }
-        catch{
-            return
-        }
+        while (!isSuccess)
         const html = response.data;
         const $ = cheerio.load(html);
         elements = $('.flex-item-mini.mx-1')
             .toArray()
             .map(x => {
-                let elementQuerrySelector = cheerio.load(x); 
+                let elementQuerrySelector = cheerio.load(x);
                 return {
-                    url: "https://mangapoisk.ru"+elementQuerrySelector(".poster-container a")[0].attribs.href,
-                    img: elementQuerrySelector(".poster-container img")?.first()[0]?.attribs["src"],                    
+                    url: "https://mangapoisk.ru" + elementQuerrySelector(".poster-container a")[0].attribs.href,
                     name: elementQuerrySelector(".poster-container img")?.first()[0]?.attribs["title"]
                 };
             });
@@ -171,106 +214,109 @@ async function GetTitle(category) {
 }
 
 async function GetTitleInfo(manga) {
-        let ua = await GetRandomUA();
-        let isSuccess = true;
-        do{
-            try{
-            var response = await delay(60000*Math.random()).then(_=> axios(manga.url));
-            }
-            catch{
-                return manga;
-            }
+    let ua = await GetRandomUA();
+    let isSuccess = true;
+    do {
+        try {
+            isSuccess = true;
+            var response = await delay(600000 * Math.random()).then(_ => axios(manga.Url));
         }
-        while(!isSuccess)
-        const html = response.data;
-        const $ = cheerio.load(html);
-        try{
+        catch {
+            isSuccess = false;
+            await delay(5000);
+        }
+    }
+    while (!isSuccess)
+    const html = response.data;
+    const $ = cheerio.load(html);
+    try {
         return {
             ...manga,
+            Img: $(".col-md-4.col-lg-4.text-center img")[0]?.attribs?.src,
             LikeCount: $("span.ratesCount")[0].firstChild.data++,
-            Rating:$(".ratingValue.ml-1")[0].firstChild.data++,
+            Rating: $(".ratingValue.ml-1")[0].firstChild.data++,
             Status: $(".post-info > span:nth-child(5)")[0].lastChild.data?.trim(),
-            Categories: $(".post-info > span:nth-child(8) > a")?.toArray()?.map(x=>x?.firstChild?.data),
+            Categories: $(".post-info > span:nth-child(8) > a")?.toArray()?.map(x => x?.firstChild?.data),
             Description: $(".manga-description.entry")[0].firstChild.data,
-            Year:$("div.post-info > span:nth-child(10)>a")[0].firstChild.data++
+            Year: $("div.post-info > span:nth-child(10)>a")[0].firstChild.data++
         }
-        }
-        catch(e){
-            console.log(e);
-            return manga;
-        }
+    }
+    catch (e) {
+        console.log(e);
+        return manga;
+    }
 }
 
 async function GetChatters(manga) {
     let isSuccess = true;
-    do{
-        try{
-        var response = await delay(60000*Math.random()).then(_=> axios(manga.url+"/chaptersList"));
+    do {
+        try {
+            var response = await delay(60000 * Math.random()).then(_ => axios(manga.Url + "/chaptersList"));
         }
-        catch{
-            return manga;
+        catch {
+            isSuccess = false;
+            await delay(5000);
         }
     }
-    while(!isSuccess)
+    while (!isSuccess)
     const html = response.data;
     const $ = cheerio.load(html);
     let chatters = $(".list-group-item.chapter-item").toArray()?.map(x => {
-        let elementQuerrySelector = cheerio.load(x); 
-        
+        let elementQuerrySelector = cheerio.load(x);
+
         let title = elementQuerrySelector(".chapter-title")[0].firstChild.data
-                                        .trim()
-                                        .replace("                    "," ");
+            .trim()
+            .replace("                    ", " ");
         return {
             CreatedAt: elementQuerrySelector(".chapter-date")[0].firstChild.data.trim(),
             Title: title,
-            url: "https://mangapoisk.ru"+elementQuerrySelector("a")[0].attribs.href
+            Url: "https://mangapoisk.ru" + elementQuerrySelector("a")[0].attribs.href
         };
     })
-    chatters.forEach(x=>RabbitMqChannel.sendToQueue(queues[1],Buffer.from(JSON.stringify(x))))
+    chatters.forEach(x => RabbitMqChannel.publish(queues[1],"mangapoisk", Buffer.from(JSON.stringify(x))))
     return {
         ...manga,
-        
-        Chaters:(Array.isArray(chatters))? chatters : []
+
+        Chaters: (Array.isArray(chatters)) ? chatters : []
     }
 }
 
 async function GetChattersImage(chatter) {
     let isSuccess = true;
-    do{
-        try{
-        var response = await delay(60000*Math.random()).then(_=> axios(chatter.url));
+    do {
+        try {
+            var response = await delay(60000 * Math.random()).then(_ => axios(chatter.Url));
         }
-        catch{
-            return;
+        catch {
+            isSuccess = false;
+            await delay(5000);
         }
     }
-    while(!isSuccess)
+    while (!isSuccess)
     const html = response.data;
     const $ = cheerio.load(html);
     chatter["Images"] = $("img.img-fluid.page-image").toArray()
-                    .map(x=>{
-                        return {
-                            Image: x.attribs["data-alternative"],
-                            Number: x.attribs["data-number"]
-                        }
-                    })
-    chatter.Images.map(x=>RabbitMqChannel.sendToQueue(queues[3],Buffer.from(JSON.stringify(x)), {
-        persistent: true
-      }))
-        
+        .map(x => {
+            return {
+                Image: x.attribs["data-alternative"],
+                Number: x.attribs["data-number"]
+            }
+        })
+
 }
-async function GetUAs(){
-    let doc = await fs.readFile(path.join(__dirname,"UserAgents.txt"));
-    return doc.toString().split('\n').map(ua=>ua.replace(' ','').replace("\r",""));
-} 
-async function GetRandomUA(){
-    
-    return uas[Math.floor(Math.random()*uas.length)];
-} 
+async function GetUAs() {
+    let doc = await fs.readFile(path.join(__dirname, "UserAgents.txt"));
+    return doc.toString().split('\n').map(ua => ua.replace(' ', '').replace("\r", ""));
+}
+async function GetRandomUA() {
+
+    return uas[Math.floor(Math.random() * uas.length)];
+}
 function delay(ms) {
     return new Promise((resolve, reject) => {
-      setTimeout(resolve, ms);
+        setTimeout(resolve, ms);
     });
-  }
-
-Parce();
+}
+if(parceMode.includes("parce")){
+    Parce();
+}
